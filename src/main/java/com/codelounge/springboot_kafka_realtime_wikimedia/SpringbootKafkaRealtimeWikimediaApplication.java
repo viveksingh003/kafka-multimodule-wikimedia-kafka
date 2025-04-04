248162eae42ca7620b2c3b4f580253812b40073c
viveksingh003 <86138699+viveksingh003@users.noreply.github.com>
2025-04-04 21:58:03 +0530

    First Commit

---
+++ src/main/java/com/codelounge/springboot_kafka_realtime_wikimedia/SpringbootKafkaRealtimeWikimediaApplication.java
@@ -0,0 +1,13 @@
+package com.codelounge.springboot_kafka_realtime_wikimedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaRealtimeWikimediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkaRealtimeWikimediaApplication.class, args);
+	}
+
+}
